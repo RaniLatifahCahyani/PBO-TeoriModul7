75f70eb169c6724d855e4ac4ed9e431a050aa1cb
RaniLatifahCahyani <114121848+RaniLatifahCahyani@users.noreply.github.com>
2022-11-03 20:00:52 +0700

    Create lat1_FOR.java

---
+++ lat1_FOR.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PBOTEORI_MODUL7;
+
+/**
+ *
+ * @author lenovo
+ */
+
+//MENGGUNAKAN FOR
+
+public class lat1_FOR {
+         public static void main(String[] args) {
+        String hari [] ={"Senin", "Selasa", "Rabu", "Kamis", "Jumat", "Sabtu", "Minggu"};
+        
+        for (int i=0; i < hari.length; i++){
+        System.out.println(hari[i]);
+        }
+    }
+}
