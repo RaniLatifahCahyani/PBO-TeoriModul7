a610dd8d4fb04c932c5273ac68a7df3588946484
RaniLatifahCahyani <114121848+RaniLatifahCahyani@users.noreply.github.com>
2022-11-03 20:01:06 +0700

    Create lat1_WHILE.java

---
+++ lat1_WHILE.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PBOTEORI_MODUL7;
+
+/**
+ *
+ * @author lenovo
+ */
+
+//MENGGUNAKAN WHILE
+
+public class lat1_WHILE {
+    public static void main(String[] args) {
+        int i = 0;
+        String days[] = {"Senin","Selasa","Rabu","Kamis","Jum'at","Sabtu","Minggu"};
+        
+        while (i < 7){
+            System.out.println(days[i]);
+            i++;
+        }
+    }
+}
+
